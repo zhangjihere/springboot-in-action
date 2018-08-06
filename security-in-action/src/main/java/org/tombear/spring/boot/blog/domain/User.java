5101f1039e6e3dc3a68a58e614cd2f6033104ae7
tombear <zhangjihere@hotmail.com>
2018-08-06 21:55:00 +0800

    add security-in-action project

---
+++ security-in-action/src/main/java/org/tombear/spring/boot/blog/domain/User.java
@@ -0,0 +1,29 @@
+package org.tombear.spring.boot.blog.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <P>Descriptions</P>
+ *
+ * @author tombear on 2018-07-24 21:59.
+ */
+//@XmlRootElement
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // entity only identity
+    private String name;
+    private String email;
+}
